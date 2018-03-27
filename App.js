3a847ad7f8a0e893e730e07cb58962ef708e613f
Sanyam Jain <sanyam.jain@myntra.com>
2018-03-27 23:51:50 +0530

    eztv: Add modal for small dialog
    * Download button is still WIP

--- App.js
+++ App.js
@@ -3,18 +3,27 @@ import React, {
 } from 'react';
 
 import {
+    Clipboard,
     Image,
     ImageBackground,
     FlatList,
     StyleSheet,
     Text,
-    View
+    View,
+    TouchableOpacity
 } from 'react-native';
+
+import Modal from "react-native-modal";
+
+import images from './src/assets/images'
 
 export default class EZTV extends Component {
     constructor(props) {
         super(props);
-        this.state = { isLoading: true };
+        this.state = {
+            isLoading: true,
+            visibleModal: false,
+        };
     }
 
     componentDidMount() {
@@ -28,12 +37,46 @@ export default class EZTV extends Component {
         });
     }
 
+    changeState(item) {
+        this.setState({
+            visibleModal: true,
+            itemData: item
+        });
+    }
+
     render() {
         return (
             <View style={{ backgroundColor: "#F6F6F6", flex: 1 }}>
-                <View style={{ backgroundColor: "white", height: 60 }}>
+                <View style={{ backgroundColor: "white", height: 50 }}>
                     <Text style={styles.heading}>eztv</Text>
                 </View>
+                {renderIf(this.state.visibleModal,
+                    <Modal isVisible={this.state.visibleModal}>
+                        <View style={styles.modalContent}>
+                            <Text style={styles.modalTitle}>{!this.state.itemData ? "" : this.state.itemData.title}</Text>
+                            <Text style={styles.modalSize}>Size: {bytesToSize(!this.state.itemData ? "" : this.state.itemData.size_bytes)} </Text>
+                            <Text style={styles.modalMarginTop}>Release Date: {convertUnixTimestampToDate(!this.state.itemData ? "" : this.state.itemData.date_released_unix)}</Text>
+                            <Text style={styles.modalMarginTop}>Seeds: {!this.state.itemData ? "" : this.state.itemData.seeds}, Peers: {!this.state.itemData ? "" : this.state.itemData.peers}</Text>
+                            <View style={styles.modalImageView}>
+                                <TouchableOpacity style={styles.modalImageDimension}
+                                    onPress={() => {
+                                        Clipboard.setString(!this.state.itemData ? "" : this.state.itemData.magnet_url)
+                                    }}>
+                                    <Image source={images.ic_magnet} style={styles.modalImageDimension} />
+                                </TouchableOpacity>
+                                <TouchableOpacity style={styles.modalImageDimension}
+                                    onPress={() => {
+                                        Clipboard.setString(!this.state.itemData ? "" : this.state.itemData.torrent_url)
+                                    }}>
+                                    <Image source={images.ic_download} style={styles.modalImageDimension} />
+                                </TouchableOpacity>
+                            </View>
+                            <TouchableOpacity onPress={() => this.setState({ visibleModal: false })}>
+                                <View> <Text style={styles.modalClose}>Close</Text> </View>
+                            </TouchableOpacity>
+                        </View>
+                    </Modal>
+                )}
                 {renderIf(this.state.isLoading,
                     <View style={styles.roundRect}>
                         <ImageBackground source={{ uri: "placeholder" }} imageStyle={{ borderRadius: 10 }} style={styles.screenshot} />
@@ -47,16 +90,23 @@ export default class EZTV extends Component {
                             return index.toString();
                         }}
                         renderItem={({ item }) =>
-                            <View style={styles.roundRect}>
-                                <ImageBackground source={{ uri: getImage(item.large_screenshot) }} imageStyle={{ borderRadius: 10 }} style={styles.screenshot}>
-                                    <View style={styles.titleBox}>
-                                        <Text style={styles.title} numberOfLines={1}>{item.title}</Text>
-                                        <Text style={styles.date}> {convertUnixTimestampToDate(item.date_released_unix)} </Text>
-                                        <Text style={styles.seedsPeers}>Seeds: {item.seeds}, Peers: {item.peers} </Text>
-                                    </View>
-                                </ImageBackground>
-                            </View>
-                        } />)}
+                            <TouchableOpacity
+                                onPress={() => {
+                                    this.changeState(item);
+                                }}>
+                                <View style={styles.roundRect}>
+                                    <ImageBackground source={{ uri: getImage(item.large_screenshot) }} imageStyle={{ borderRadius: 10 }} style={styles.screenshot}>
+                                        <View style={styles.titleBox}>
+                                            <Text style={styles.title} numberOfLines={1}>{item.title}</Text>
+                                            <Text style={styles.date}> {convertUnixTimestampToDate(item.date_released_unix)} </Text>
+                                            <Text style={styles.seedsPeers}>Seeds: {item.seeds}, Peers: {item.peers} </Text>
+                                        </View>
+                                    </ImageBackground>
+                                </View>
+                            </TouchableOpacity>
+                        }
+                    />
+                )}
             </View>
         )
     }
@@ -99,9 +149,17 @@ function convertUnixTimestampToDate(timestamp) {
     return time;
 };
 
+function bytesToSize(bytes) {
+    var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    if (bytes == 0) return '0 Byte';
+    var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+    return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
+};
+
 const styles = StyleSheet.create({
     heading: {
         fontSize: 24,
+        fontWeight: "bold",
         marginTop: 20,
         textAlign: "center"
     },
@@ -145,5 +203,39 @@ const styles = StyleSheet.create({
         right: 0,
         paddingRight: 10,
         paddingBottom: 5
+    },
+    modalContent: {
+        backgroundColor: "white",
+        padding: 22,
+        borderRadius: 4,
+        alignItems: 'center',
+        borderColor: "rgba(0, 0, 0, 0.1)"
+    },
+    modalTitle: {
+        fontSize: 20,
+        fontWeight: "bold"
+    },
+    modalSize: {
+        marginTop: 15
+    },
+    modalMarginTop: {
+        marginTop: 5
+    },
+    modalImageView: {
+        marginTop: 15,
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: 100
+    },
+    modalImageDimension: {
+        height: 35,
+        width: 35
+    },
+    modalClose: {
+        fontSize: 16,
+        fontWeight: "bold",
+        marginTop: 50,
+        width: 40
     }
 });
---
+++ src/assets/images/index.js
@@ -0,0 +1,4 @@
+export default images = {
+    'ic_download': require('./ic_download.png'),
+    'ic_magnet': require('./ic_magnet.png'),
+}
